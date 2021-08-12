aefa988540ca78dec176136a5af68532c573694b
Brady Bott <bottbrady@gmail.com>
2021-08-12 12:04:07 -0600

    still fixing my css

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ const path = require('path')
 const app = express()
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')))
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../styles.css')))
+app.get('/styles', (req, res) => res.sendFile(path.join(__dirname, '../styles.css')))
+app.get('/images', (req, res) => res.sendFile(path.join(__dirname, '../images')))
 
 const port = process.env.PORT || 5050
 
